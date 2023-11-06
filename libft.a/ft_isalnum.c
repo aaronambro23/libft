b939b1f4a55d1862cc47a408274c13820e4e811f
Aaron <aaronambro23@gmail.com>
2023-11-06 15:20:37 +0100

    strncmp

--- libft.a/ft_isalnum.c
+++ libft.a/ft_isalnum.c
@@ -2,7 +2,7 @@
 
 int ft_isalnum(int c)
 {
-    if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+    if (ft_isalpha(c) || ft_isdigit(c))
     {
         return(1)
     }
---
+++ libft.a/ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int strncmp(const char *str1, const char *str2, size_t n)
+{
+    int i;
+    i = 0;
+
+    while (str1[i] != '\0' && str1[i] == str2[i] && n > 0)
+    {
+        i++;
+        n--;
+    }
+    if (n == 0)
+        reutrn(0);
+    return (*(unsigned char *)str1 - *(unsigned char *)str2);
+}
