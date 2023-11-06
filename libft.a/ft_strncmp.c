261fc6c6ca423754b3862b0c0ef53c46cd882cd0
Aaron <aaronambro23@gmail.com>
2023-11-06 16:34:15 +0100

    done

--- libft.a/ft_strncmp.c
+++ libft.a/ft_strncmp.c
@@ -11,6 +11,6 @@ int strncmp(const char *str1, const char *str2, size_t n)
         n--;
     }
     if (n == 0)
-        reutrn(0);
+        return(0);
     return (*(unsigned char *)str1 - *(unsigned char *)str2);
 }
