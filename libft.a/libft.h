4caf1972dba2678557b34163acb60c5373c16cb7
Aaron <aaronambro23@gmail.com>
2023-11-06 16:49:45 +0100

    atoi

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -1,3 +1,5 @@
+int	ft_atoi(const char *str);
+
 void ft_bzero(void *s, size_t n);
 
 int ft_isalnum(int c);
@@ -21,6 +23,8 @@ size_t ft_strlcat(char *dest, const char *src, size_t size);
 size_t ft_strlcpy(char *dest, const char *src, size_t size);
 
 size_t ft_strlen(const char *str);
+
+int strncmp(const char *str1, const char *str2, size_t n);
 
 char *ft_strrchr(const char *str, int c);
 
