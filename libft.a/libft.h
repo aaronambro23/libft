cbd8269a20bf7f9e49ad406a820cc0aabcaece2c
Aaron <aaronambro23@gmail.com>
2023-11-14 01:33:07 +0100

    Added: putstr, putchar, putendl

--- libft.a/libft.h
+++ libft.a/libft.h
@@ -26,6 +26,12 @@ void *ft_memmove(void *s1, const void *s2, size_t n);
 
 void *ft_memset(void *s, int c, size_t n);
 
+void	ft_putchar_fd(char c, int fd);
+
+void	ft_putendl_fd(char *s, int fd);
+
+void	ft_putstr_fd(char *s, int fd);
+
 char	**ft_split(char const *str, char c);
 
 char *ft_strchr(const char *str, int c);
