dd42f8b962eb1da57af394c640c16d9bd504ee08
Aaron <aaronambro23@gmail.com>
2023-12-05 00:38:46 +0100

    trim fixes

--- libft.a/ft_strtrim.c
+++ libft.a/ft_strtrim.c
@@ -22,14 +22,37 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (s1 != 0 && set != 0)
 	{
 		i = 0;
-		j = ft_strlen(s1);
+		j = ft_strlen(s1) - 1;
 		while (s1[i] && ft_strchr((char *)set, s1[i]))
 			i++;
-		while (s1[j - 1] && ft_strchr((char *)set, s1[j - 1]) && j > i)
+		while (s1[j] && ft_strchr((char *)set, s1[j]) && j > i)
 			j--;
 		str = (char *)malloc(sizeof(char) * (j - i + 1));
 		if (str)
 			ft_strlcpy(str, &s1[i], j - i + 1);
 	}
 	return (str);
+}
+
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	end = ft_strlen(s1) - 1;
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		++start;
+	while (s1[end] && ft_strchr(set, s1[end]))
+		--end;
+	trimmed = ft_substr(s1, start, end - start + 1);
+	return (trimmed);
 }
