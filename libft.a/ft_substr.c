5da2a773877ba34b56fd335ca7fb3106bbff80dd
Aaron <aaronambro23@gmail.com>
2023-12-05 01:04:11 +0100

    substr

--- libft.a/ft_substr.c
+++ libft.a/ft_substr.c
@@ -34,3 +34,40 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	}
 	return (str);
 }
+
+
+/*
+** Function: ft_substr
+** --------------------
+** Returns a substring from the string 's'. The substring begins at index 'start'
+** and is of maximum size 'len'.
+**
+** s: the string from which to create the substring.
+** start: the index in string 's' where the substring starts.
+** len: the maximum length of the substring.
+**
+** returns: the newly created substring. NULL if the string 's' is NULL or if memory allocation fails.
+*/
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;  // iterator for the loop
+	char	*str;  // the substring to be returned
+
+	// if the string is NULL, return NULL
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s)) 	// if the start index is greater than the length of the string, return an empty string
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start)) 	// if the length of the substring is greater than the length of the string starting from 'start', adjust 'len'
+		len = ft_strlen(s + start);
+	str = ft_calloc(len + 1, sizeof(char)); 
+	if (!str) 	// if memory allocation fails, return NULL
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i]; 	// copy 'len' characters from 's' starting from 'start' to 'str'
+		i++;
+	}
+	return (str); 	// return the substring
+}
